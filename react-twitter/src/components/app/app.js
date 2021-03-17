4b1dd48246e113b137f645b2538c830840b0a494
Olga Barishevskaya <bariolga@yandex.ru>
2021-03-17 14:22:21 +0300

    twitter app basic structure + styles

--- react-twitter/src/components/app/app.js
+++ react-twitter/src/components/app/app.js
@@ -1,7 +1,12 @@
 import React from 'react';
+
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
-//import ReactDOM from 'react-dom';
+import PostStatusFilter from '../post-status-filter';
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
+
+import './app.css';
 
 const App = () => {
 	return (
@@ -9,7 +14,10 @@ const App = () => {
 			<AppHeader />
 			<div className="search-panel d-flex">
 				<SearchPanel/>
+				<PostStatusFilter/>
 			</div>
+			<PostList/>
+			<PostAddForm/>
 		</div>
 	);
 };
