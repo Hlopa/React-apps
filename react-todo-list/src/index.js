15cfeb0ddef134b632db30430a1acc2fd5bdaab6
Olga Barishevskaya <bariolga@yandex.ru>
2021-03-17 13:24:48 +0300

    start twitter-react app

--- react-todo-list/src/index.js
+++ react-todo-list/src/index.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const el = <h1>Hello world!!</h1>
+const TodoList = () => {
+	return (
+		<ul>
+			<li>Learn React</li>
+			<li>Build Awersome App</li>
+		</ul>
+	);
+};
+
+const AppHeader = () => {
+	const divSryle = {
+		backgroundColor: "red"
+	};
+	return <h1 style={divSryle}>My Todo List</h1>;
+};
+
+const SearchPanel = () => <input placeholder='search' />;
+
+const App = () => {
+	return (
+		<div>
+			<AppHeader />
+			<SearchPanel />
+			<TodoList />
+		</div>
+	);
+};
+
 
 ReactDOM.render(
-  el,
-  document.getElementById('root')
+	<App />,
+	document.getElementById("root")
 );
+
