2906f2ca30526d4dbd9b3226368d9ee774e96645
Olga Barishevskaya <bariolga@yandex.ru>
2021-03-30 23:07:35 +0300

    finish you tube app

--- you-tube/src/components/App.js
+++ you-tube/src/components/App.js
@@ -11,6 +11,10 @@ export default class App extends Component {
     selectedVideo: null
   };
 
+  componentDidMount(){
+    this.searchVideos('space')
+  }
+
   searchVideos = async (term) => {
     const response = await axios.get('/search/', {
       params: {
@@ -18,19 +22,32 @@ export default class App extends Component {
       }
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({ 
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   }
 
   onVideoSelect = (video) => {
-    this.setState({selectedVideo : video})
+    this.setState({ selectedVideo: video })
   }
 
   render() {
     return (
       <div className='ui container'>
         <SearchBar onFormSubmit={this.searchVideos} />
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList videos={this.state.videos} onVideoSelect = {this.onVideoSelect}/>
+        <div className='ui grid'>
+          <div className='ui row'>
+            <div className='eleven wide column'>
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className='five wide column'>
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect} />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
