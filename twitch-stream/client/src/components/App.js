3121e6fb527159f1f4c403caccfa45716b5578b7
Olga Barishevskaya <bariolga@yandex.ru>
2021-04-10 19:41:28 +0300

    finish interaction with data

--- twitch-stream/client/src/components/App.js
+++ twitch-stream/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Router, Route} from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 
 import Header from './Header';
 import StreamList from './streams/StreamList';
@@ -13,16 +13,18 @@ import history from '../history';
 const App = () => {
   return (
     <div className='ui container'>
-      
+
       <Router history={history}>
-         <div>
-            <Header/>
-           <Route path='/'exact component={StreamList}></Route>
-           <Route path='/streams/new' exact component={StreamCreate}/>
-           <Route path='/streams/edit/:id' exact component={StreamEdit}/>
-           <Route path='/streams/delete/:id' exact component={StreamDelete}/>
-           <Route path='/streams/show' exact component={StreamShow}/>
-         </div>
+        <div>
+          <Header />
+          <Switch>
+            <Route path='/' exact component={StreamList}></Route>
+            <Route path='/streams/new' exact component={StreamCreate} />
+            <Route path='/streams/edit/:id' exact component={StreamEdit} />
+            <Route path='/streams/delete/:id' exact component={StreamDelete} />
+            <Route path='/streams/:id' exact component={StreamShow} />
+          </Switch>
+        </div>
       </Router>
     </div>
   )
